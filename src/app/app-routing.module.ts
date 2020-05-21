98e9832858cf00747e1dd6c1182547e76d31b018
Simo Helenius <srthelenius@outlook.com>
2020-05-21 09:53:38 +0300

    History component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { FrontPageComponent } from './front-page/front-page.component';
 import { AddTaskComponent } from './add-task/add-task.component';
+import { HistoryComponent } from './history/history.component';
 
 import { AuthGuard } from './auth/auth.guard';
 
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'front-page', component: FrontPageComponent},
   { path: 'add-task', component: AddTaskComponent},
+  { path: 'history', component: HistoryComponent},  
   { path: '',   redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login'},
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
 import { LoginComponent } from './auth/login/login.component';
 import { HeaderComponent } from './header/header.component';
 import { FrontPageComponent } from './front-page/front-page.component';
 import { AuthGuard } from './auth/auth.guard';
 import { AddTaskComponent } from './add-task/add-task.component';
 import { CalendarComponent } from './calendar/calendar.component';
+import { HistoryComponent } from './history/history.component';
 
 @NgModule({
   declarations: [
@@ -19,13 +21,15 @@ import { CalendarComponent } from './calendar/calendar.component';
     HeaderComponent,
     FrontPageComponent,
     AddTaskComponent,
-    CalendarComponent
+    CalendarComponent,
+    HistoryComponent
   ],
   imports: [
+    CommonModule,
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule
+    ReactiveFormsModule
   ],
   providers: [ AuthGuard ],
   bootstrap: [AppComponent]
