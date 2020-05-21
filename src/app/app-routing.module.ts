db3fd9fb74c7c033060ca9911cb22e2d2a8439f9
Simo Helenius <srthelenius@outlook.com>
2020-05-21 08:21:57 +0300

    added auth, header and f-p comps

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './auth/login/login.component';
 
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '',   redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login'},
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './auth/login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FrontPageComponent } from './front-page/front-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    FrontPageComponent
   ],
   imports: [
     BrowserModule,
